9883cb093c742e750cf1582acd0936985dc9e8bc
Tanya <ta.rohlova@yandex.ru>
2021-10-30 21:00:47 +0300

    изменение state

--- src/App.js
+++ src/App.js
@@ -13,6 +13,19 @@ class App extends Component {
     pageTitle: 'React components'
   }
 
+  changeTitleHandler = () => {
+
+    const oldTitle = this.state.pageTitle
+
+    const newTitle = oldTitle + ' (changed)'
+    
+    //метод для изменения состояния state
+    this.setState({
+      pageTitle: newTitle
+    })
+
+  }
+
   render() {
     const divStyle = {
       textAlign: 'center'
@@ -23,6 +36,8 @@ class App extends Component {
     return (
       <div style={divStyle}>
         <h1>{this.state.pageTitle}</h1>
+
+        <button onClick={this.changeTitleHandler}>Change title</button>
 
         <Car name={cars[0].name} year={cars[0].year} />
         <Car name={cars[1].name} year={cars[1].year} />
